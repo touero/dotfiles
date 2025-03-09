d7ecc8386953b35866b95985f1e0b5a28c54eb2f
touero <touer0018@gmail.com>
2025-03-09 11:08:13 +0800

    fix(nvim): yazi plugin and add gitignore

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("notify").setup({background_colour = "#000000"})
 vim.env.HTTP_PROXY = "http://127.0.0.1:7890"
 vim.env.HTTPS_PROXY = "http://127.0.0.1:7890"
