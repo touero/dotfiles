8df3e77cf224be67670abdb5689324ab83a6cf9e
Ensong Wei <touer0018@gmail.com>
2025-03-15 13:07:28 +0800

    Fix vim proxy (#14)
    * fix: alias
    
    * fix(vim): proxy
    
    Support all driver proxy at home, so not set proxy local proxy any more.
    Judging by family network segment.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.env.HTTP_PROXY = "http://127.0.0.1:7890"
-vim.env.HTTPS_PROXY = "http://127.0.0.1:7890"
+local handle = io.popen("ip route | grep 'default' | awk '{print $3}'")
+local gateway = ""
+
+if handle then
+    gateway = handle:read("*a") or ""
+    handle:close()
+end
+
+if gateway:match("^192%.168%.6%.") then
+    vim.env.HTTP_PROXY = "http://127.0.0.1:7890"
+    vim.env.HTTPS_PROXY = "http://127.0.0.1:7890"
+end
