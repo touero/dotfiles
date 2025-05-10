c5aaf4e3915e055c610a27c3138c93860c7b6420
Ensong Wei <touer0018@gmail.com>
2025-05-10 16:12:54 +0800

    fix: mason.vim api error, config linespace and format lua (#25)
    code
    
    - temporarily fix mason.vim mapping api error
    - fix line height in wezterm, it will affect all in terminal
    - format wezterm lua code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("notify").setup({background_colour = "#000000"})
+vim.opt.linespace = 13
+vim.g.lazyvim_check_order = false
 local handle = io.popen("ip route | grep 'default' | awk '{print $3}'")
 local gateway = ""
 
 if handle then
-    gateway = handle:read("*a") or ""
-    handle:close()
+  gateway = handle:read("*a") or ""
+  handle:close()
 end
 
 if gateway:match("^192%.168%.6%.") then
-    vim.env.HTTP_PROXY = "http://127.0.0.1:7890"
-    vim.env.HTTPS_PROXY = "http://127.0.0.1:7890"
+  vim.env.HTTP_PROXY = "http://127.0.0.1:7890"
+  vim.env.HTTPS_PROXY = "http://127.0.0.1:7890"
 end
---
+++ .config/nvim/lua/plugins/mason-lspconfig-fix.lua
@@ -0,0 +1,5 @@
+return {
+  "williamboman/mason-lspconfig.nvim",
+  version = vim.fn.has("nvim-0.11") == 1 and false or "1.32.0",
+  config = function() end,
+}
