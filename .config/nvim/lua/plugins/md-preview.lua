c38f8271799d2a01b73554e3ab92b1452ad701f3
touero <touer0018@gmail.com>
2025-05-11 13:27:22 +0800

    fix(nvim): md preview and keymap
    Only in md file `r` can use MarkdownPreviewToggle

--- .config/nvim/lua/plugins/md-preview.lua
+++ .config/nvim/lua/plugins/md-preview.lua
@@ -1,8 +1,12 @@
 return {
   "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
   ft = { "markdown" },
+  config = function()
+    vim.keymap.set("n", "r", "<cmd>MarkdownPreviewToggle<CR>", { buffer = true, desc = "MarkdownPreviewToggle" })
+  end,
 }
