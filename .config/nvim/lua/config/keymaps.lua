ddd4ac1a5297ee23ad7a333f74f8f808487be17d
touero <touer0018@gmail.com>
2025-05-30 17:10:23 +0800

    fix(nvim): neovim.vscode extension extra

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", {})
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "jkf", "<Esc>:wq<CR>", { silent = true })
 vim.api.nvim_set_keymap("i", "<Tab>", 'pumvisible() ? "<C-n>" : "<Tab>"', { expr = true })
 
@@ -14,3 +14,12 @@ vim.api.nvim_set_keymap("n", "<S-Enter>", ":normal O<CR>", {})
 
 vim.api.nvim_set_keymap("v", "<Tab>", ">gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+
+if vim.g.vscode then
+  vim.api.nvim_set_keymap(
+    "n",
+    "<leader>f",
+    "<Cmd>call VSCodeNotify('workbench.action.gotoSymbol')<CR>",
+    { noremap = true, silent = true }
+  )
+end
