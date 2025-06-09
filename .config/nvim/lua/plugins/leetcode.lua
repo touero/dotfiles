2ea408c06fca9c13d4386b8dfff792bca24e1fe0
touero <touer0018@gmail.com>
2025-06-09 17:43:39 +0800

    feat(nvim): new plugin leetcode for nvim and fix alias

---
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,62 @@
+---@alias lc.lang
+---| "cpp"
+---| "java"
+---| "python"
+---| "python3"
+---| "c"
+---| "csharp"
+---| "javascript"
+---| "typescript"
+---| "php"
+---| "swift"
+---| "kotlin"
+---| "dart"
+---| "golang"
+---| "ruby"
+---| "scala"
+---| "rust"
+---| "racket"
+---| "erlang"
+---| "elixir"
+---| "bash"
+
+---@alias lc.hook
+---| "enter"
+---| "question_enter"
+---| "leave"
+
+---@alias lc.size
+---| string
+---| number
+---| { width: string | number, height: string | number }
+
+---@alias lc.position "top" | "right" | "bottom" | "left"
+
+---@alias lc.direction "col" | "row"
+
+---@alias lc.inject { before?: string|string[], after?: string|string[] }
+
+---@alias lc.storage table<"cache"|"home", string>
+
+---@alias lc.picker { provider?: "fzf-lua" | "telescope" }
+
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    ---@type string
+    arg = "lt",
+    ---@type lc.lang
+    lang = "python",
+    cn = {
+      enabled = true, ---@type boolean
+      translator = true, ---@type boolean
+      translate_problems = true, ---@type boolean
+    },
+  },
+}
