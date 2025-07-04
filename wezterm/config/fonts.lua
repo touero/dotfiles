75703ffab3122425a1c774b2731567350eb0db56
touero <touer0018@gmail.com>
2025-07-04 15:10:26 +0800

    fix: wezterm font and add line height, starship remove bg

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -10,9 +10,10 @@ return {
     { family = "Noto Sans SC:style=Regular" },
     { family = "Noto Sans Mono CJK SC" },
   }),
-  font_size = 16,
+  font_size = 14,
+  line_height = 1.1,
 
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
-  freetype_load_target = "Normal", ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
-  freetype_render_target = "Normal", ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+  freetype_load_target = "Light", ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+  freetype_render_target = "Light", ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
 }
