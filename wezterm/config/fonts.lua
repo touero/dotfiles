3ea1bcef6be6c6a2e43e8beeea27530092de6022
touero <touer0018@gmail.com>
2025-07-11 15:10:43 +0800

    fix(wezterm): font

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -2,11 +2,11 @@ local wezterm = require("wezterm")
 
 return {
   font = wezterm.font_with_fallback({
+    { family = "Fira Code", weight = "Regular" },
+    { family = "JetBrainsMono Nerd Font", weight = "Regular" },
     { family = "JetBrains Mono" },
     { family = "MesloLGS NF" },
     { family = "Monaca" },
-    { family = "JetBrainsMono Nerd Font", weight = "Regular" },
-    { family = "FiraCode Nerd Font" },
     { family = "Noto Sans SC:style=Regular" },
     { family = "Noto Sans Mono CJK SC" },
   }),
@@ -15,4 +15,5 @@ return {
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
   freetype_load_target = "Light", ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
   freetype_render_target = "Light", ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
+  harfbuzz_features = { "cv12", "cv01", "cv02", "cv13", "ss05" },
 }
