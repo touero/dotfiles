ae4c52c4a9769fc31731c851fd22da175aab0b7d
touero <touer0018@gmail.com>
2025-09-06 22:28:26 +0800

    fix(wezterm, scope): wezterm keybinding and scope env

--- .config/wezterm/config/bindings.lua
+++ .config/wezterm/config/bindings.lua
@@ -123,7 +123,6 @@ local keys = {
       patterns = {
         "https?://\\S+",
       },
-      skip_action_on_paste = true,
       action = wezterm.action_callback(function(window, pane)
         local url = window:get_selection_text_for_pane(pane)
         wezterm.log_info("opening: " .. url)
