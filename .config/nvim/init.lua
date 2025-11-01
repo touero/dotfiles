0312ca957bf8b7436cf61a530d5d3044c338c490
touero <touer0018@gmail.com>
2025-11-01 15:19:52 +0800

    fix(nvim): update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,15 +3,27 @@ require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
 vim.opt.linespace = 13
 vim.g.lazyvim_check_order = false
-local handle = io.popen("ip route | grep 'default' | awk '{print $3}'")
-local gateway = ""
-
+vim.opt.guicursor = table.concat({
+  "n-v-c:block", -- 普通模式：方块
+  "i-ci-ve:ver25", -- 插入模式：竖线
+  "r-cr:hor20", -- 替换模式：下划线
+  "o:hor50", -- 选择模式：下划线
+}, ",")
+local handle = io.popen("ip route | awk '/default/ {print $3}'")
+local gateway = handle and handle:read("*a"):match("%S+") or ""
 if handle then
-  gateway = handle:read("*a") or ""
   handle:close()
 end
 
 if gateway:match("^192%.168%.6%.") then
-  vim.env.HTTP_PROXY = "http://127.0.0.1:7890"
-  vim.env.HTTPS_PROXY = "http://127.0.0.1:7890"
+  vim.env.HTTP_PROXY = nil
+  vim.env.HTTPS_PROXY = nil
+  vim.env.http_proxy = nil
+  vim.env.https_proxy = nil
+else
+  local proxy = "http://127.0.0.1:7890"
+  vim.env.HTTP_PROXY = proxy
+  vim.env.HTTPS_PROXY = proxy
+  vim.env.http_proxy = proxy
+  vim.env.https_proxy = proxy
 end
--- .config/nvim/lua/plugins/mason-lspconfig-fix.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "williamboman/mason-lspconfig.nvim",
-  version = vim.fn.has("nvim-0.11") == 1 and false or "1.32.0",
-  config = function() end,
-}
