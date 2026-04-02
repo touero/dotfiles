b941a2351ca97c39929a0ffec837e969a2cebe86
touero <touer0018@gmail.com>
2026-04-02 09:55:56 +0800

    fix(nvim): md preview style and concealment

--- .config/nvim/lua/plugins/md-preview.lua
+++ .config/nvim/lua/plugins/md-preview.lua
@@ -4,9 +4,23 @@ return {
   build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+
+    vim.g.mkdp_auto_close = 1
+
+    vim.g.mkdp_preview_options = {
+      sync_scroll_type = "middle",
+      hide_yaml_meta = 1,
+      disable_sync_scroll = 0,
+    }
+
+    vim.g.mkdp_page_title = "「${name}」"
+
+    vim.g.mkdp_theme = "dark"
   end,
   ft = { "markdown" },
   config = function()
     vim.keymap.set("n", "r", "<cmd>MarkdownPreviewToggle<CR>", { buffer = true, desc = "MarkdownPreviewToggle" })
+
+    vim.opt_local.updatetime = 100
   end,
 }
