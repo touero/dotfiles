01792808a10f808ba9f8217bf0835fdc74721829
touero <ensongwei@gmail.com>
2026-06-29 15:57:37 +0800

    fix(nvim): terminal plugin

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -2,63 +2,32 @@ return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
-    opts = {
-      direction = "float",
+    event = "VeryLazy",
 
+    opts = {
+      open_mapping = [[<C-\>]],
+      direction = "float",
       shading_factor = 0,
       shade_terminals = false,
       start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
       persist_size = true,
       persist_mode = true,
 
       float_opts = {
         border = "rounded",
-
         width = function()
           return math.floor(vim.o.columns * 0.8)
         end,
-
         height = function()
           return math.floor(vim.o.lines * 0.8)
         end,
-
         winblend = 0,
-
         highlights = {
           border = "FloatBorder",
           background = "NormalFloat",
         },
-      },
-    },
-
-    keys = {
-      {
-        "<c-\\>",
-        "<cmd>ToggleTerm<cr>",
-        mode = "n",
-        desc = "toggle floating terminal",
-      },
-
-      {
-        "<c-w>",
-        function()
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-\\><c-n>", true, false, true), "n", false)
-
-          vim.cmd("wincmd p")
-        end,
-        mode = "t",
-        desc = "leave terminal focus",
-      },
-
-      {
-        "<c-\\>",
-        function()
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<c-\\><c-n>", true, false, true), "n", false)
-
-          vim.cmd("ToggleTerm")
-        end,
-        mode = "t",
-        desc = "toggle floating terminal",
       },
     },
 
@@ -96,10 +65,35 @@ return {
         })
       end
 
+      local function set_terminal_keymaps()
+        local keymap_opts = {
+          buffer = 0,
+          silent = true,
+        }
+
+        vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], keymap_opts)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], keymap_opts)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], keymap_opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], keymap_opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], keymap_opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], keymap_opts)
+      end
+
       set_toggleterm_highlight()
 
+      local group = vim.api.nvim_create_augroup("ToggleTermConfig", {
+        clear = true,
+      })
+
       vim.api.nvim_create_autocmd("ColorScheme", {
+        group = group,
         callback = set_toggleterm_highlight,
+      })
+
+      vim.api.nvim_create_autocmd("TermOpen", {
+        group = group,
+        pattern = "term://*toggleterm#*",
+        callback = set_terminal_keymaps,
       })
     end,
   },
