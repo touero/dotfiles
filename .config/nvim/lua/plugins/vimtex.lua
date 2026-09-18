2b5be0796d08dc9fb3987bbfeaf1d7958ff11e4e
touero <ensongwei@gmail.com>
2026-09-18 14:37:50 +0800

    fix(nvim): use skim view pdf in mac

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -31,6 +31,8 @@ return {
     if is_mac then
       latexmk_config.executable = "/Library/TeX/texbin/latexmk"
       vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
     else
       vim.g.vimtex_view_method = "zathura"
     end
